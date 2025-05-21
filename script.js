47ed2135fda9be8be4b84da3737a0d16466697ca
BayraktarM <MIKAIL.BAYRAKTAR@latoile.em-lyon.com>
2025-05-21 12:43:48 +0200

    commit1

---
+++ script.js
@@ -0,0 +1,14 @@
+fetch("https://js-dynamic-portfolio-data-makerslab-emlyon-cdweb-8f83155c64a0cc.gitlab.io/json/vtc.json")
+.then(reponse => reponse.json())
+.then(data=>{
+    console.log(data);
+    
+})
+const container = document.getElementById('nomCommercial');
+const cont= document.getElementById('h1')
+console.log(container);
+function afficherProduits(){ 
+ <h1>${nomCommercial}</h1>;}
+ 
+// const card = document.createElement('div');
+  
